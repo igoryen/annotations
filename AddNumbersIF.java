ba87321eaa9e4c3551d450e60caf5e57b931b4e0
igoryen <igor.yentaltsev@gmail.com>
2014-07-08 22:01:31 -0400

    AddNumbersIF v2

--- AddNumbersIF.java
+++ AddNumbersIF.java
@@ -1,8 +1,9 @@
-//1
+//2
 @WebService(targetNamespace = "http://duke.example.org",
         name = "AddNumbers")
 public interface AddNumbersIF extends Remote {
 
+    @WebMethod(operationName = "add", action = "urn:addNumbers")
     public int addNumbers(int number1, int number2) throws 
             RemoteException, AddNumbersException;
 
