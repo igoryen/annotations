af20be9f8ae7ad9945e0864894d382823dcde0a8
igoryen <igor.yentaltsev@gmail.com>
2014-07-08 21:46:53 -0400

    v1

--- a.java
+++ a.java
@@ -1,1 +1,17 @@
 v1
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface WebService {
+    String name() default "";
+
+    String targetNamespace() default "";
+
+    String serviceName() default "";
+
+    String wsdlLocation() default "";
+
+    String endpointInterface() default "";
+
+    String portName() default "";
+}
